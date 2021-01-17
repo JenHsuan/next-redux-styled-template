33e5010e09a0ff73f75fbd4ca795601e11517022
JenHsuan <ofalpha@gmail.com>
2021-01-17 14:22:24 +0800

    fix test cases for redux-promise-middleware

--- tests/__tests__/components/ReduxTester.test.js
+++ tests/__tests__/components/ReduxTester.test.js
@@ -3,26 +3,36 @@ import { Provider } from "react-redux";
 import ReduxTester from "../../../components/ReduxTester";
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import thunk from 'redux-thunk';
+import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from '../../../components/reducers';
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-function renderWithProviders(ui, initialState = {}) {
-  const middleware = [thunk];
-  const store = createStore(
+//actions
+import {
+  getData
+} from "../../../components/actions/action";
+
+function renderWithProviders(ui, store) {
+  return render(<Provider store={store}>{ui}</Provider>);
+}
+
+function getStore(initialState = {}) {
+  const middleware = [promiseMiddleware];
+  return createStore(
     rootReducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
   );
-  return render(<Provider store={store}>{ui}</Provider>);
 }
 
 
 describe('Component: ReduxTester', () => {
-  test('should render with given state from Redux store', () => {
+  test('should render with given state from Redux store', async() => {
     let mockedData = 'Test component';
-    const { getByTestId } = renderWithProviders(<ReduxTester />)
+    let store = getStore();
+    await store.dispatch(getData());
+    const { getByTestId } = renderWithProviders(<ReduxTester />, store)
     expect(getByTestId("text-content")).toHaveTextContent(mockedData)
   });
 });
